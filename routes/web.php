09b3c234dca4c837e94373d16afa63724e5737c8
suprit-infinity-plus1 <suprit@infinityplus1.in>
2026-01-03 19:32:22 +0530

    filter code added

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,9 @@
 
 use App\Http\Controllers\Auth\GoogleController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\User\CardController;
 use App\Http\Controllers\User\ChatController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\User\CardController;
-
 
 Route::get('/', function () {
     return view('index');
@@ -28,7 +27,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-
     Route::prefix('dashboard')->group(function () {
         // =========================
         // USER CARDS ROUTES  ✅
@@ -45,15 +43,9 @@ Route::middleware('auth')->group(function () {
             ->name('user.chat.index');
     });
 
-
 });
 
 Route::get('/auth/google', [GoogleController::class, 'redirect'])->name('google.login');
 Route::get('/auth/google/callback', [GoogleController::class, 'callback']);
 
-
-
-
-
-
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
